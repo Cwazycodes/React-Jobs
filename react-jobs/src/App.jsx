26de6228b9590389a1672318c640d12d04c6d476
Muhammad Hussain <muhammadhussain@muhammads-mbp.home>
2024-08-05 22:38:41 +0100

    router setup

--- react-jobs/src/App.jsx
+++ react-jobs/src/App.jsx
@@ -1,3 +1,4 @@
+import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom'
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
@@ -5,16 +6,10 @@ import HomeCards from "./components/HomeCards";
 import JobListings from "./components/JobListings";
 import ViewAllJobs from "./components/ViewAllJobs";
 
+const router = createBrowserRouter(createRoutesFromElements(<Route index element={<h1>My App</h1>} />))
+
 const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <HomeCards />
-      <JobListings />
-      <ViewAllJobs />
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
